a7a173abb3df3f515a07b54ec2d4bb036184e12f
Nanderson Matheus Reis <nandersonmatheusmelo@gmail.com>
2023-10-07 21:28:51 -0300

    project stard and relationship configuration

---
+++ app/Models/Tweet.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+    use HasFactory;
+
+    protected $filable = [
+        'content'
+    ];
+
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+}
